fb4ce59e09040c7e139f740224eee72e178cc71c
Joseba Juániz <joseba.juaniz@gmail.com>
2015-07-19 16:28:29 +0200

    Initial commit

---
+++ Module.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace atuin\routes;
+
+
+class Module extends \atuin\skeleton\ComposerModule
+{
+    protected static $_id = 'atuin-menus';
+
+    protected static $_version = '0.0
+    
+    ';
+    /**
+     * Checks if is a core module, atuin won't be able to uninstall or deactivate this module
+     *
+     * @var int
+     */
+    public $is_core_module = 1;
+
+    /**
+     * Used from AppManagement, determines if the module will be loaded in the backend zone.
+     *
+     * @var int
+     */
+    public $is_backend = 1;
+    /**
+     * Used from AppManagement, determines if the module will be loaded in the frontend zone.
+     *
+     * @var int
+     */
+    public $is_frontend = 1;
+
+    protected $composerPackage = 'cyneek/yii2-menu';
+
+    protected $composerVersion = 'dev-master';
+
+}
---
+++ config/AtuinConfig.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace atuin\routes\config;
+
+use atuin\config\models\ModelConfig;
+
+
+/**
+ * Class ConfigSkeleton
+ * @package common\engine\module_skeleton\libraries
+ *
+ * Class called to install a module in the CMS.
+ *
+ * Here must be all the automatic changes in the system that will be necessary to install a new module.
+ *
+ */
+class AtuinConfig extends \atuin\skeleton\config\AtuinConfig
+{
+
+    /**
+     * @inheritdoc
+     */
+    public function upMigration()
+    {
+
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function downMigration()
+    {
+
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function upMenu()
+    {
+
+    }
+
+
+    /**
+     * @inheritdoc
+     */
+    public function downMenu()
+    {
+
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function upConfig()
+    {
+    }
+
+
+    /**
+     * @inheritdoc
+     */
+    public function downConfig()
+    {
+        $this->configItems->deleteConfig();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function upManual()
+    {
+        // delete neo user that automatically the migration adds
+
+    }
+
+
+    /**
+     * @inheritdoc
+     */
+    public function downManual()
+    {
+
+    }
+
+}
